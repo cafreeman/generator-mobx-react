4a12a16e3c11012dd02c6339ba28139329013aa6
Chris Freeman <cfreeman.code@gmail.com>
2016-04-01 14:43:36 -0500

    minor changes to demo app
    `index.js` now creates a `div` element in the html file created by webpack.
    
    Added a `main.css` file to illustrate how the webpack config will handle css when building

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import Store from './Store';
 import App from './components/App';
 
+const root = document.createElement('div');
+root.id = 'app';
+document.body.appendChild(root);
+
 const store = new Store();
 
 ReactDOM.render(
