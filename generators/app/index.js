de01de4c2c0573c4a487c5b547b7b2b356cecb27
Chris Freeman <cfreeman.code@gmail.com>
2016-04-17 11:12:35 -0500

    Housekeeping
    Cleaned up a lot of messy code.
    
    Renamed all the template files to to be prefixed with `_` to make things  a little more clear, and also moved them to a `src_es2015` directory instead of just `src` to prep for the TypeScript config option

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,68 +3,63 @@ const yeoman = require('yeoman-generator');
 
 module.exports = yeoman.Base.extend({
   prompting() {
-    let done = this.async();
+    const done = this.async();
     this.prompt([
       {
         type: 'input',
         name: 'name',
         message: 'Your project name',
         // Defaults to the project's folder name if not specified
-        default: this.appname
+        default: this.appname,
       },
       {
         type: 'input',
         name: 'description',
         message: 'Please enter a description for your project',
-        default: ''
+        default: '',
       },
       {
         type: 'confirm',
         name: 'installDeps',
         message: 'Would you like to install all dependencies now?',
-        default: false
-      }
-    ], function (answers) {
+        default: false,
+      },
+    ], (answers) => {
       this.props = answers;
       done();
-    }.bind(this));
+    });
   },
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('package.json'),
+      this.templatePath('_package.json'),
       this.destinationPath('package.json'), {
         name: this.props.name,
-        description: this.props.description
+        description: this.props.description,
+        devDeps,
       }
     );
 
+    // .gitignore
     this.fs.copy(
-      this.templatePath('gitignore'),
+      this.templatePath('_gitignore'),
       this.destinationPath('.gitignore')
     );
 
-    let filesToCopy = [
-      '.eslintrc.js',
-      'webpack.config.js'
-    ];
-
-    filesToCopy.forEach(file => {
-      this.fs.copyTpl(
-        this.templatePath(file),
-        this.destinationPath(file),
-        {
-          name: this.props.name
-        }
+    // set up eslint if using es2015
+    if (this.props.language === 'es2015') {
+      this.fs.copy(
+        this.templatePath('_eslintrc.js'),
+        this.destinationPath('.eslintrc.js')
       );
-    });
+    }
 
     this.fs.copyTpl(
       this.templatePath('src'),
       this.destinationPath('src'),
       {
         name: this.props.name,
-        description: this.props.description
+        description: this.props.description,
       }
     );
   },
@@ -76,5 +71,5 @@ module.exports = yeoman.Base.extend({
       this.log(`Skipping the install step. Run \`npm install\` inside the project root when
         you're ready.`);
     }
-  }
+  },
 });
